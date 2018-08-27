cae00cbd662c35119b23e0027ead94146a7210e6
amalonex10 <amalonex10@gmail.com>
2018-08-27 18:17:05 -0500

    Section1

---
+++ Section1/Section1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section1
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            DisplayMenu();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("A)dd Movie");
+            Console.WriteLine("E)dit Movie");
+            Console.WriteLine("D)elete Movie");
+            Console.WriteLine("V)iew Movies");
+            Console.WriteLine("Q)uit");
+
+            string input = Console.ReadLine();
+            switch (input [0])
+            {
+                case 'A': AddMovie(); break;
+                case 'E': EditMovie(); break;
+                case 'D': DeleteMovie(); break;
+                case 'V': ViewMovie(); break;
+                case 'Q': ; break;
+
+                default: Console.WriteLine("Please enter a valid value."); break;
+            }
+        }
+
+        private static void AddMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EditMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DeleteMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ViewMovie()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
