d79b8a52552a1d76e3468c3999cef6482f046a97
amalonex10 <amalonex10@gmail.com>
2018-11-28 19:00:12 -0600

    changes

--- Classwork/Section2/Movie.Mvc/Controllers/MovieController.cs
+++ Classwork/Section2/Movie.Mvc/Controllers/MovieController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ITSE1430.MovieLib;
+using ITSE1430.MovieLib.Sql;
+using Movie.Mvc.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +12,85 @@ namespace Movie.Mvc.Controllers
 {
     public class MovieController : Controller
     {
+        public MovieController ()
+        {
+            var connString = ConfigurationManager.ConnectionStrings["MovieDatabase"];
+
+            _database = new SqlMovieDatabase(connString.ConnectionString);
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var items = _database.GetAll();
+
+            return View(items.Select(i => new MovieModel(i)));
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new MovieModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(MovieModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var item = model.ToDomain();
+
+                    _database.Add(item);
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                }
+
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit( string id )
+        {
+            var item = _database.GetAll().FirstOrDefault(i => i.Name == id);
+
+            return View(new MovieModel(item));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(MovieModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var item = model.ToDomain();
+
+                    var exsisting = _database.GetAll().FirstOrDefault(i => i.Name == model.Name);
+
+                    _database.EditCore(exsisting.Name, item);
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                }
+
+            };
+
+            return View(model);
+        }
+
+        private readonly IMovieDatabase _database;
     }
 }
