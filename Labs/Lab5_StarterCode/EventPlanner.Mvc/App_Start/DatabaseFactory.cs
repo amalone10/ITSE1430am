c627da4a8acdccbc9e641a3eff8645738db1f661
amalonex10 <amalonex10@gmail.com>
2018-12-09 18:50:33 -0600

    not good

---
+++ Labs/Lab5_StarterCode/EventPlanner.Mvc/App_Start/DatabaseFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.App_Start
+{
+    public class DatabaseFactory
+    {
+        private static EventDatabase()
+        { }
+    }
+}
---
+++ Labs/Lab5_StarterCode/EventPlanner.Mvc/Models/EventPlanner.Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.Models
+{
+    public class EventPlanner
+    {
+        public bool IncludePublic { get; set; }
+
+        public bool IncludePrivate { get; set; }
+
+        public DateTime? BeginDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
